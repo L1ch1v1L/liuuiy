8db0a8ede40f27095e50ae9bf49f1b276c6e93cf
SkrillXXx <ebanutuijh@mail.ru>
2024-09-23 10:04:57 +0500

    full code

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
+<?php
+include_once "gen.php"
+?>
 <!DOCTYPE HTML>
 <html>
     <head>
+        <link href="style.css" rel="stylesheet">
         <title>привет новости тут</title>
     </head>
     <body>
-    <?php
-    ?>
+        <h1>Текущие новости в tproger</h1>
+        <?php
+		gen_table();
+        ?>
     </body>
 </html>
